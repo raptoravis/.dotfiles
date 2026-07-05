4691b3c6f35ba12f0c0afb7647f3d280fb0dfd55
raptoravis <raptoravis@gmail.com>
2026-07-05 10:46:35 +0800

    reasonix-y

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -126,6 +126,7 @@ Set-Alias -Name profile -Value profile_alias
 function reload_alias { & $PROFILE }
 Set-Alias -Name reload -Value reload_alias
 
+Set-Alias reasonix-y "reasonix --yolo"
 # ---------------------------------------------------------------------------
 # Navigation: goto <shortcut>
 # ---------------------------------------------------------------------------
